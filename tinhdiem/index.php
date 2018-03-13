95e512f99391f903afaa24b854532d324c6f7b4c
Doan Minh Tuan <tuan.doanminh@gameloft.com>
2018-03-13 11:25:49 +0700

    add new file

---
+++ tinhdiem/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<header id="main-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<img src="chu.png" alt="logo"></img>
+			</div>
+		</div>
+	</div>
+</header>
+</body>
+</html>
